1ff7b34e05a0a7ac4b3cce99da5a21d0bbb01b2c
andytxr <andreluiiz.gst@gmail.com>
2022-07-05 15:32:12 -0300

    Changing the 'as_json' method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,11 @@
 class Contact < ApplicationRecord
+
+    def author
+        "Deco"
+    end
+
+    def as_json(options={})
+        super(methods: :author, root: true, status: :partial_content)
+    end
+
 end
