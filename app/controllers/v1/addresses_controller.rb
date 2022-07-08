9e6d4487b76ece0da1e8de3a7ae3c7b3d867bc5e
andytxr <andreluiiz.gst@gmail.com>
2022-07-08 17:10:10 -0300

    Fixing

---
+++ app/controllers/v1/addresses_controller.rb
@@ -0,0 +1,63 @@
+module v1
+
+    class AddressesController < ApplicationController
+        before_action :set_address, only: [:show, :update, :create]
+
+        def show
+        
+            render json: @address
+        
+        end
+
+        def create
+
+            @contact.address = Address.new(address_params)
+
+            if (@contact.save)
+                
+                render json: @contact.address, status: :created, location: contact_address_url(@contact)
+            else
+
+                render json: @contact.address.errors, status: :unprocessable_entity
+
+            end
+
+        end
+
+        def update
+
+            if(@contact.address.update(address_params))
+
+                render json: @contact.address
+
+            else
+
+                render json: @contact.errors, status: :unprocessable_entity
+
+            end
+
+        end 
+
+        def destroy
+        
+            @contact.address.destroy 
+
+        end
+
+        private
+
+            def set_address
+
+                @address = Contact.find(params[:contact_id]).address
+            
+            end
+
+            def address_params
+
+                ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+
+            end
+            
+    end
+
+end
---
+++ app/controllers/v1/phones_controller.rb
@@ -0,0 +1,68 @@
+module v1
+
+    class PhonesController < ApplicationController
+        
+        before_action :set_phone, only: [:show, :update, :create]
+
+        def show
+        
+            render json: @contact.phones
+        
+        end
+
+        def create
+
+            @contact.phones << Phone.new(phone_params)
+
+            if (@contact.save)
+                
+                render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+            else
+
+                render json: @contact.phones.errors, status: :unprocessable_entity
+
+            end
+
+        end
+
+        def update
+            
+            phone = Phone.find(phone_params[:id])
+            
+
+            if(phone.update(phone_params))
+
+                render json: @contact.phones
+
+            else
+
+                render json: @contact.errors, status: :unprocessable_entity
+
+            end
+
+        end 
+
+        def destroy
+            
+            phone = Phone.find(phone_params[:id])
+            phone.destroy 
+
+        end
+
+        private
+
+            def set_phone
+
+                @phone = Contact.find(params[:contact_id]).phone
+            
+            end
+
+            def phone_params
+
+                ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+
+            end
+
+    end
+
+end
